71573ac64093a95d575845c197ad197a34d510d2
wu <526989270@qq.com>
2016-06-29 00:25:27 +0800

    The first week has completed Net Knight's initial UI.Others and  details will complete in the next week.

--- app/src/main/java/com/pencilbox/netknight/view/MainIp.java
+++ app/src/main/java/com/pencilbox/netknight/view/MainIp.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.Toast;
 
