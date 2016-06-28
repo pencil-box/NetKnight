057d06f4e40ca1e20a8c8ab5758b0a297483add9
wu <526989270@qq.com>
2016-06-28 22:47:29 +0800

    修复完善listview图标显示

---
+++ app/src/main/java/com/pencilbox/netknight/view/IDairyView.java
@@ -0,0 +1,15 @@
+package com.pencilbox.netknight.view;
+
+/**
+ * Created by wu on 16/6/28.
+ */
+
+public interface IDairyView {
+
+    void showaUseofNet();
+
+    void showUseofWifi();
+
+    void showUseofNetwifi();
+
+}
