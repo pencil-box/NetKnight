1404a23a8c6bc01a744964b326b1cf246036a37d
wu <526989270@qq.com>
2016-06-29 10:26:14 +0800

    了接口

---
+++ app/src/main/java/com/pencilbox/netknight/presentor/DairyImpl.java
@@ -0,0 +1,26 @@
+package com.pencilbox.netknight.presentor;
+
+import android.widget.BaseAdapter;
+import com.pencilbox.netknight.view.IDairyView;
+
+/**
+ * Created by wu on 16/6/28.
+ */
+
+public class DairyImpl {
+    //选择显示的时间数据用量
+    public void showUseOfWifi(long id, int time) {
+
+    }
+    public void showUseOfCelluar(long id,int time){
+
+    }
+    public void showUseOfWifi_Celluar(long id ,int time){
+
+    }
+
+    //加载选择的表图
+    public void loadDatachart() {
+
+    }
+}
