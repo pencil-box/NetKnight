060758ba335026080119d3f52f9495b6824c52cc
Abel Su <abel1995@163.com>
2016-07-10 03:30:02 +0800

    modify IDairyPresenter

--- app/src/main/java/com/pencilbox/netknight/presentor/IDairyPresenter.java
+++ app/src/main/java/com/pencilbox/netknight/presentor/IDairyPresenter.java
@@ -1,16 +1,18 @@
 package com.pencilbox.netknight.presentor;
 
+import com.github.mikephil.charting.charts.LineChart;
+
 /**
  * Created by wu on 16/6/29.
  */
 
 public interface IDairyPresenter {
     //选择显示的时间数据用量
-    void showUseOfWifi(long id, long recordTime);
+    void showUseOfWifi(LineChart lineChart);
 
-    void showUseOfCelluar(long id, long recordTime);
+    void showUseOfCelluar(LineChart lineChart);
 
-    void showUseOfWifiCelluar(long id, long recordTime);
+    void showUseOfWifiCelluar(LineChart lineChart);
 
 
     //加载选择的表图
